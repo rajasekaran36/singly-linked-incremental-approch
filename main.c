0396a27e8d971f12f2930442519338f1013d0319
Rajasekaran S <proffraja@gmail.com>
2025-03-16 22:39:16 +0530

    dynamic memmory node

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 struct node {
     int data;
     struct node *next;
@@ -9,5 +10,11 @@ int main(){
     n1.data = 10;
     n1.next = NULL;
     printf("%d",n1.data);
+
+    node *n2; 
+    n2 = (node*)malloc(sizeof(node));
+    n2->data = 20;
+    n2->next = NULL;
+    printf("\n%d",n2->data);
     return 0;
 }
