4dafedbd5063bbc9435b69eb7b70620c010d52fa
Rajasekaran S <proffraja@gmail.com>
2025-03-16 22:04:51 +0530

    node created & tested

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
+struct node {
+    int data;
+    struct node *next;
+};
+typedef struct node node;
 int main(){
-    printf("Hello World");
+    node n1;
+    n1.data = 10;
+    n1.next = NULL;
+    printf("%d",n1.data);
     return 0;
 }
