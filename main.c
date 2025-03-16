ba2cee2925d116d466149446d51e9fc4ea28bca8
Rajasekaran S <proffraja@gmail.com>
2025-03-16 23:13:12 +0530

    traverse function

--- main.c
+++ main.c
@@ -5,20 +5,35 @@ struct node {
     struct node *next;
 };
 typedef struct node node;
+node *head, *current;
 node* create_node(int data){
     node *new_node = (node*)malloc(sizeof(node));
     new_node->data = data;
     new_node->next = NULL;
     return new_node;
 }
+void traverse(){
+    current = head;
+    while(current!=NULL){
+        printf("%d->",current->data);
+        current = current->next;
+    }
+    printf("NULL");
+}
 int main(){
-    node *n1,*n2;
+    int i;
+    node *n1,*n2,*n3,*n4;
     n1 = create_node(10);
     n2 = create_node(20);
-
+    n3 = create_node(30);
+    n4 = create_node(40);
+    
     n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
 
-    printf("%d",n1->data);
-    printf("\n%d",n1->next->data);
+    head = n1;
+    traverse();
+    
     return 0;
 }
